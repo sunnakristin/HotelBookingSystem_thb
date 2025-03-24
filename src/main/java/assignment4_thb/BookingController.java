35723a964a4fc35c1d4dc6abc0990d4195b442ed
jonasbjarki <jbb27@hi.is>
2025-03-24 10:57:04 +0000

    Booking, BookingController, Payment, booking-view.fxml

--- src/main/java/assignment4_thb/BookingController.java
+++ src/main/java/assignment4_thb/BookingController.java
@@ -1,7 +1,9 @@
 package assignment4_thb;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 
@@ -17,25 +19,34 @@ public class BookingController {
     private DatePicker checkIn;
     @FXML
     private Button payButton;
+    @FXML
+    private Button confirmButton;
+    @FXML
+    private ChoiceBox<String> cashOrCard;
 
     public BookingController(Booking booking){
         this.booking = booking;
     }
     public void initialize(){
         if(booking != null){
-            this.typeLabel.setText(booking.getRoom().getPrice());
+            this.typeLabel.setText(String.valueOf(booking.getRoom().getPrice()));
             this.priceLabel.setText(booking.getRoom().getType());
+            cashOrCard.setItems(FXCollections.observableArrayList("Cash", "Card"));
+            cashOrCard.setValue("Cash");
         }
     }
     public void confirm(){
         if (checkIn.getValue() != null && checkOut.getValue() != null) {
             booking.confirm(checkIn.getValue(), checkOut.getValue());
             payButton.setDisable(false);
+            confirmButton.setDisable(true);
+            cashOrCard.setDisable(false);
         } else {
             System.out.println("Please select both check-in and check-out dates");
         }
     }
     public void pay(){
         booking.getPayment().process();
+
     }
 }

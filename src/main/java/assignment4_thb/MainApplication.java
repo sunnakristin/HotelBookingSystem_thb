e2fa6c2ff8561c75d0fef2d12c1d9e6ba6422785
Katrinanna <katrinaasmunds@gmail.com>
2025-03-31 10:39:29 +0000

    eyddi dóti

--- src/main/java/assignment4_thb/MainApplication.java
+++ src/main/java/assignment4_thb/MainApplication.java
@@ -1,4 +1,4 @@
-package assignment4_thb;
+package assignment4_thb.vidmot;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("room-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Login!");
+        stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,8 @@
 module com.example.assignment4_thb {
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.desktop;
 
 
-    opens assignment4_thb to javafx.fxml;
-    exports assignment4_thb;
+    opens com.example.assignment4_thb to javafx.fxml;
+    exports com.example.assignment4_thb;
 }

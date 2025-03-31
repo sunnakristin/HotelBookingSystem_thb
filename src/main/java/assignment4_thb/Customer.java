3ed877155e6b9060d12deda5bb7d7898c899cdec
jonasbjarki <jbb27@hi.is>
2025-03-31 10:38:09 +0000

    Customer

--- src/main/java/assignment4_thb/Customer.java
+++ src/main/java/assignment4_thb/Customer.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class Customer {
     private ArrayList<Booking> bookings;
+
     public void addBooking(Booking booking){
         bookings.add(booking);
     }

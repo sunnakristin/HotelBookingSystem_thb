5e587943468bd89a7b2ca897fdafa1d5f7cb9eab
sunnakristin <sunnakristing@gmail.com>
2025-04-07 15:20:02 +0000

    Uppfært DatabaseManager klasi svo læsist ekki

--- src/main/java/assignment4_thb/Customer.java
+++ src/main/java/assignment4_thb/Customer.java
@@ -10,9 +10,6 @@ public class Customer {
     private String email;
     private String password;
     private ObservableList<Booking> bookings;
-    //private String preferredLanguage;
-    //private String cardInfo;
-    //private List<Review> reviews;
 
     public void addBooking(Booking booking){
         bookings.add(booking);
@@ -37,52 +34,12 @@ public class Customer {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    /*
-    public String getPreferredLanguage() {
-        return preferredLanguage;
-    }
-
-    public void setPreferredLanguage(String preferredLanguage) {
-        this.preferredLanguage = preferredLanguage;
-    }
-
-    public String getCardInfo() {
-        return cardInfo;
-    }
-
-    public void setCardInfo(String cardInfo) {
-        this.cardInfo = cardInfo;
-    }
-
-     */
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public boolean verifyPassword(String password) {
         return this.password.equals(password);
     }
 
-    /*
-    public void addReview(Review review) {
-        reviews.add(review);
-    }
-
-    public List<Review> getReviews() {
-        return reviews;
-    }
-
-     */
 }
